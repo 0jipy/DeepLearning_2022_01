6f9f26c8f7613b1c0ab5ce6050c0cdde04e99918
0jipy <96100083+0jipy@users.noreply.github.com>
2022-01-12 17:17:07 +0900

    20줄 첫프로그래밍. 지지직

--- 01.Basic/boston2.py
+++ 01.Basic/boston2.py
@@ -1,42 +1,33 @@
 # 입력값으로 0에서 100사이의 정수값을 받아서 
 # 실제 가격과 보스톤 주택가격의 실제값과 예측값을 보여주는 프로그램 
+# 모델을 학습하지 않고 저장된 모델 활용
+
 
 # import part
 import numpy as np 
 import pandas as pd
 import warnings
-import tensorflow as tf
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import load_model
 
 # 상수값 설정 등 변수 초기화
 seed = 2022
+model_filename = 'boston.h5'
 warnings.filterwarnings('ignore')
 np.random.seed(seed)
-tf.random.set_seed(seed)  
 boston = load_boston()
 X_train, X_test, y_train, y_test = train_test_split(
     boston.data, boston.target, test_size = 0.1, random_state=seed
 )
 
-# 메인 모델 만들기 , 메인모델을 만들것이 아니라 미리 저장해두고 가져올것.
-# 학습한 모델 자체를 save해서 이용할것. 미리학습모델 저장. 사용자 요청으로 바로 불러 사용하는 것.
-model = Sequential([
-    Dense(30, input_dim=13, activation='relu'),
-    Dense(12, activation='relu'),
-    Dense(1)    # 회귀라서 활성화 함수 없어
-])
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, validation_split=0.1, epochs=500, batch_size=60, verbose=0)
+# 저장된 메인 모델 불러오기
+model = load_model(model_filename)
 
-
-# 1. 입력값 받기
+# 입력값 받기
 index = int(input('0 ~ 50 정수값을 입력하세요.> '))
 test = X_test[index].reshape(1,-1)
 pred_value = model.predict(test)
-
 
 # 최종 결과 출력
 print(f'실제값: {y_test[index]}, 예측값 : {pred_value[0,0]:.2f}')
---
+++ 01.Basic/boston3.py
@@ -0,0 +1,49 @@
+# 입력값으로 0에서 100사이의 정수값을 받아서 
+# 실제 가격과 보스톤 주택가격의 실제값과 예측값을 보여주는 프로그램 
+# 모델을 학습하지 않고 저장된 모델 활용
+# 사용법변경 3 : python boston3.py 20 (test_dataset_index) 2> /dev/null
+
+# import part(부분)
+import sys
+import numpy as np 
+import warnings
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model
+
+# argument 정리 # 미입력에 대한 처리 완료 # 잘못임력에 대한 처리
+if len(sys.argv) <= 1:           # argument 미입력
+    print(sys.argv[0], len(sys.argv))
+    print('사용법 : python boston3.py test_dataset_index(0~50) 2> /dev/null')
+    sys.exit()
+try:
+    index = int(sys.argv[1])
+except:
+    print('정수를 입력하세요')
+    print('사용법 : python boston3.py test_dataset_index(0~50) 2> /dev/null')
+    sys.exit()
+
+if index < 0 or index > 50:
+    print('0과 50사이의 정수를 입력하세요')
+    print('사용법 : python boston3.py test_dataset_index(0~50) 2> /dev/null')
+    sys.exit()
+
+# 상수값 설정 등 변수 초기화
+seed = 2022
+model_filename = 'boston.h5'
+warnings.filterwarnings('ignore')
+np.random.seed(seed)
+boston = load_boston()
+X_train, X_test, y_train, y_test = train_test_split(
+    boston.data, boston.target, test_size = 0.1, random_state=seed
+)
+
+# 저장된 메인 모델 불러오기
+model = load_model(model_filename)
+
+# 입력값 받기
+test = X_test[index].reshape(1,-1)
+pred_value = model.predict(test)
+
+# 최종 결과 출력
+print(f'실제값: {y_test[index]}, 예측값 : {pred_value[0,0]:.2f}')
