00b8f35866a9a25115e006085bea0dbac8608019
0jipy <96100083+0jipy@users.noreply.github.com>
2022-01-30 16:05:52 +0900

    continu

--- 91.보강/hello.py
+++ 91.보강/hello.py
@@ -1,8 +1,9 @@
 from sqlite3 import paramstyle
 
 
-money = False
-if money:
-    print('택시')
-else:
-    print("걸어가")
+treeHit = 0
+while treeHit < 10:
+    treeHit= treeHit +1
+    print(f"나무를 {treeHit:d}번 찍었습니다.")
+    if treeHit == 10:
+        print("나무 넘어갑니다")
