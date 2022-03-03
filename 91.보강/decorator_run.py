14e82367a8ce1e858f7041650ef4b731a9de78c3
0jipy <96100083+0jipy@users.noreply.github.com>
2022-03-03 23:57:17 +0900

    0303 1157

---
+++ 91.보강/decorator_run.py
@@ -0,0 +1,38 @@
+def deco(fun):
+    def wrapper():
+
+        print(3)
+
+        print(2)
+
+        fun()
+    
+    return wrapper
+
+
+# youngwhan() 
+
+
+
+var = 1
+
+temp = var + 1
+
+def youngwhan():
+    print('youngwhan')
+
+
+
+
+
+def minsoo():
+    print('minsoo')
+
+
+
+
+
+
+pass
+
+
---
+++ 91.보강/decotest.py
@@ -0,0 +1,28 @@
+def extract_data_dart(soup):
+
+
+def extract_data_naverFiance(soup):
+
+    try:
+        print(0)
+    except:
+        pass
+
+def extract_data_invest(soup):
+
+
+def deco(fun):
+
+    def anyName:
+        try:
+            fun()
+        except:
+            pass  # 데코 함수 내부에서 정의된 함수 리턴해야 한다.
+
+    return anyName
+
+
+##########################################3
+
+# 소스코드의 재사용성을 위해 데코레이터 사용. 
+# 데코레이터의 작동원리 .
